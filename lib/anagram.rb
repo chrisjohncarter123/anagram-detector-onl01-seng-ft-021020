59ba60da74022c606caa818bd32edd140748bdb5
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-21 03:37:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,44 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :input
+  def initialize(input)
+    @input = input
+  end
+  
+  def match(array)
+    
+    test_hash = {
+      
+    }
+    
+    input.split.each_with_index do |letter, index|
+      if(test_hash.)
+    end
+    
+    array.each_with_index do |word, index|
+      
+      t = true
+      h = {
+        
+      }
+      word.split("").each_with_index do |letter, index|
+
+        
+        if(input.split("").include?(letter) == false)
+          t = false
+        end
+        
+      end
+      if(t == true)
+        puts word
+      end
+
+    end
+
+  end
+end
+
+a = Anagram.new("listen")
+
+a.match(%w(enlists google inlets banana))
